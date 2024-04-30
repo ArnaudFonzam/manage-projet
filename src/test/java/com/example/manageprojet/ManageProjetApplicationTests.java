56a2ba1e20f6d905e1bfa977a85b6c04b38f2115
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2024-04-30 18:11:35 +0100

    setup the projet

---
+++ src/test/java/com/example/manageprojet/ManageProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manageprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
